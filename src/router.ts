2ee424ea2d39068fd2646352eeb08ebfc247336a
Jacob Ernst <akumadude.je@gmail.com>
2023-10-06 14:17:44 -0500

    Creating withShortLink middleware and removing unneeded endpoints

--- src/router.ts
+++ src/router.ts
@@ -12,20 +12,7 @@ type CF = [env: Env, context: ExecutionContext];
 // now let's create a router (note the lack of "new")
 const router = Router();
 
-// GET collection index
-router.get('/api/todos', () => new Response('Todos Index!'));
-
-// GET item
-router.get('/api/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
-
-// POST to the collection (we'll use async here)
-router.post('/api/todos', async (request) => {
-  const content = await request.json();
-
-  return new Response(`Creating Todo: ${JSON.stringify(content)}`);
-});
-
-router.get<IRequest, CF>('/sl/:slug', withParams, async (request, env) => {
+async function withShortLink(request: IRequest, env: Env) {
   const shortLink = await env.SHORT_LINKS.get<ShortLink>(request.params.slug, {
     type: 'json',
   });
@@ -33,6 +20,12 @@ router.get<IRequest, CF>('/sl/:slug', withParams, async (request, env) => {
   if (!shortLink) {
     return new Response('Not Found.', { status: 404 });
   }
+  request.shortLink = shortLink;
+}
+
+// GET short-link and redirect.
+router.get<IRequest, CF>('/:slug', withParams, withShortLink, async (request) => {
+  const shortLink = request.shortLink as ShortLink;
 
   const override = handleOverrides(request, shortLink.overrides || []);
   if (override) {
